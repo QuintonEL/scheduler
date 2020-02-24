7059b8e61afcdf333906899d1fee499590a3c723
QuintonEL <quintonlaborde@gmail.com>
2020-02-24 16:29:33 -0700

    added test for deleting

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -82,13 +82,19 @@ export default {
       });
     }
   }),
-  
+
   put: jest.fn(url => {
-    /* Resolve appointments data */
     return Promise.resolve({
       status: 204,
       statusText: "No Content"
     });
-  })
+  }),
+
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
+  }),
 
 };
